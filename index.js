904451bcc401ef19127eb7b6f6432acccd812775
shaymoria <shai.moria@zooz.com>
2017-07-09 15:50:57 +0300

    After second code review

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ function validateOptions(options) {
     if (options.callback && typeof options.callback !== 'function') {
         throw new Error('callback must be a function and must return a Promise');
     }
-    if (options.newConnectionsTimeout && (isNaN(options.newConnectionsTimeout) || options.newConnectionsTimeout <=0)) {
+    if (options.newConnectionsTimeout && (isNaN(options.newConnectionsTimeout) || Number(options.newConnectionsTimeout) <= 0)) {
         throw new Error('newConnectionsTimeout must be a positive number');
     }
-    if (isNaN(options.shutdownTimeout) || options.shutdownTimeout <= 0) {
+    if (isNaN(options.shutdownTimeout) || Number(options.shutdownTimeout) <= 0) {
         throw new Error('shutdownTimeout is required and must be a number greater then 0');
     }
     if (!options.server) {
